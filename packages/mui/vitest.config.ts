b194ad8ac9ebf344757fe829294b8a5cebcb6571
ryuseiyarou <nittaa@hotmail.co.jp>
2026-08-30 16:50:12 +0900

    test: increase MUI Vitest timeout

--- packages/mui/vitest.config.ts
+++ packages/mui/vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "jsdom",
-    setupFiles: [fileURLToPath(new URL("./test/setup.ts", import.meta.url))]
+    setupFiles: [fileURLToPath(new URL("./test/setup.ts", import.meta.url))],
+    testTimeout: 15000
   }
 });
